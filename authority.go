816fb3dc1485b9e85c3d4d2898d80b0c908fa2cc
tiancheng.xu <kiritoeva@icloud.com>
2024-05-15 09:12:41 +0800

    feat: 新增权限修改操作

--- authority.go
+++ authority.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/Yostardev/gf"
 	"github.com/Yostardev/requests"
+	"strconv"
 	"time"
 )
 
@@ -20,6 +21,7 @@ type authorityResponse struct {
 	Msg  string     `json:"msg"`
 	Code int        `json:"code"`
 }
+
 type Authority struct {
 	Id            int       `json:"id"`
 	CreatedAt     time.Time `json:"created_at"`
@@ -29,6 +31,12 @@ type Authority struct {
 	Obj           string    `json:"obj"`
 	Act           string    `json:"act"`
 	ApplicationId int       `json:"application_id"`
+}
+
+type authorityListResponse struct {
+	Data []*Authority `json:"data"`
+	Msg  string       `json:"msg"`
+	Code int          `json:"code"`
 }
 
 func (c *Client) CreateAuthority(name, describe, obj, act string) (*Authority, error) {
@@ -48,6 +56,69 @@ func (c *Client) CreateAuthority(name, describe, obj, act string) (*Authority, e
 	}
 
 	var resp authorityResponse
+	err = res.Body.JsonBind(&resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Data, nil
+}
+
+func (c *Client) UpdateAuthority(id int, name, describe, obj, act string) (*Authority, error) {
+	res, err := requests.New().SetUrl(gf.StringJoin(c.serverURL, "/ump/api/v1/authority/", strconv.Itoa(id))).SetJsonBody(&authorityRequest{
+		Name:          name,
+		Describe:      describe,
+		ApplicationID: c.applicationID,
+		Obj:           obj,
+		Act:           act,
+	}).AddHeader("Authorization", c.token).Put()
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("update authority failed, status code: %d, response data: %s", res.StatusCode, res.Body.String())
+	}
+
+	var resp authorityResponse
+	err = res.Body.JsonBind(&resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Data, nil
+}
+
+func (c *Client) DeleteAuthority(id int) error {
+	res, err := requests.New().SetUrl(gf.StringJoin(c.serverURL, "/ump/api/v1/authority/", strconv.Itoa(id))).
+		AddHeader("Authorization", c.token).
+		Delete()
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != 200 {
+		return fmt.Errorf("delete authority failed, status code: %d, response data: %s", res.StatusCode, res.Body.String())
+	}
+
+	return nil
+}
+
+func (c *Client) GetAuthorityByObjAndAct(obj, act string) ([]*Authority, error) {
+	res, err := requests.New().SetUrl(gf.StringJoin(c.serverURL, "/ump/api/v1/authority/all")).
+		AddQuery("application_id", strconv.Itoa(c.applicationID)).
+		AddQuery("obj", obj).
+		AddQuery("act", act).
+		AddHeader("Authorization", c.token).Get()
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("get authority failed, status code: %d, response data: %s", res.StatusCode, res.Body.String())
+	}
+
+	var resp authorityListResponse
 	err = res.Body.JsonBind(&resp)
 	if err != nil {
 		return nil, err
--- go.mod
+++ go.mod
@@ -1,15 +1,17 @@
 module github.com/Yostardev/ump-sdk
 
-go 1.20
+go 1.22
+
+toolchain go1.22.3
 
 require (
-	github.com/Yostardev/gf v1.0.3
-	github.com/Yostardev/requests v1.0.4
+	github.com/Yostardev/gf v1.0.4
+	github.com/Yostardev/requests v1.0.6
 )
 
 require (
-	github.com/Yostardev/json v1.0.5 // indirect
-	github.com/bytedance/sonic v1.10.2 // indirect
+	github.com/Yostardev/json v1.0.6 // indirect
+	github.com/bytedance/sonic v1.11.3 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
@@ -20,4 +22,5 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	gopkg.in/twindagger/httpsig.v1 v1.2.0 // indirect
 )
