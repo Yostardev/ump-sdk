d73e512a714c0a2efdabf63325429f2086f337f6
tiancheng.xu <kiritoeva@icloud.com>
2025-08-25 13:20:28 +0800

    refactor: 使用 resty 替换 requests 库并更新相关代码

--- example/main.go
+++ example/main.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	url   = "https://dev-opsump.yostar.net"
-	token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ4dXRpYW5jaGVuZyIsIm5hbWUiOiLlvpDlpKnmiJAiLCJpc3MiOiJZb1N0YXIgVXNlciBNYW5hZ2UgUGxhdGZvcm0iLCJleHAiOjE3NTYzNDU4MTIsIm5iZiI6MTc1NjA4NjYxMiwiaWF0IjoxNzU2MDg2NjEyLCJqdGkiOiI0NzkwMGJjNi0zMjk2LTQ2ZTgtYWY0NC0yYTRiNTFiNTYxZTcifQ.EVrjwKxdA58pIvbYK6y7LSQjUVL_Ml5KaSMS5OlON3Vp2nulZ7NneojHssJAIi5-OtgAQXRKSV77dt4F4K377A"
+	token = ""
 )
 
 func main() {
