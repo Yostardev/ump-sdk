8944c36283b17cc513ec8b662c8b832ec68aebc4
tiancheng.xu <kiritoeva@icloud.com>
2025-09-03 14:49:35 +0800

    refactor: 使用 sonic 替换默认的 JSON 序列化和反序列化库

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package ump_sdk
 import (
 	"time"
 
+	"github.com/bytedance/sonic"
 	"github.com/go-resty/resty/v2"
 )
 
@@ -15,6 +16,8 @@ func NewClient(url string, applicationID int, token string) *Client {
 	return &Client{
 		applicationID: applicationID,
 		restyClient: resty.New().
+			SetJSONMarshaler(sonic.Marshal).
+			SetJSONUnmarshaler(sonic.Unmarshal).
 			SetTimeout(10*time.Second).
 			SetAuthToken(token).
 			SetAuthScheme("").
